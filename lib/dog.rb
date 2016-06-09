cf4be303d752fcdcdac6824b9e5594e487a85cb1
Yichen Yang <yichen.yang@flatironschool.com>
2016-06-09 01:10:28 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,8 @@
+#1
+class Dog
+  #definition here
+end
+
+fido = Dog.new
+snoopy = Dog.new
+lassie = Dog.new
---
+++ lib/person.rb
@@ -0,0 +1,6 @@
+class Person
+  #define a person class
+end
+
+adele_goldberg = Person.new
+alan_kay = Person.new
